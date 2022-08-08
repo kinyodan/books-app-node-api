a5aaf3ef566880253eb71d627873d527471df908
Daniel kinyo <9253824-kinyodan@users.noreply.gitlab.com>
2022-08-08 11:43:06 +0300

    first commit

---
+++ src/controllers/books.ts
@@ -0,0 +1,74 @@
+import { Request, Response} from 'express';
+import axios, { AxiosResponse } from 'axios';
+
+// Start api endpoints ----------------------------
+//
+
+const getBooksList = async (req: Request, res: Response) => {
+
+    // get books list
+    let result: AxiosResponse = await axios.get(`https://anapioficeandfire.com/api/books`);
+    let booksList = result.data;
+    return res.status(200).json({
+        status: true,
+        message: "books listed ",
+        data: booksList
+    });
+};
+
+const getBook = async (req: Request, res: Response) => {
+    // set the book id
+    let url = req.query.url;
+
+    // get the Book items data
+    let result: AxiosResponse = await axios.get(`${url}`);
+    let book = result.data;
+
+    return res.status(200).json({
+        status: true,
+        message: "books listed ",
+        data: book,
+    });
+};
+
+const getBookCharacters = async (req: Request, res: Response) => {
+
+    let book_character_urls = req.body;
+    let characters_list: any = []
+    characters_list = await threadGetCharacter(book_character_urls)
+    return res.status(200).json({
+        status: true,
+        message: "Characters listed ",
+        data: characters_list,
+    });
+};
+
+//
+// end api endpoints ----------------------------
+
+// helper methods -------------------------------
+//
+
+const dataList: any  = []
+
+async function threadGetCharacter(characters: any) {
+    for (const character of characters) {
+        try {
+            let result: AxiosResponse = await axios.get(`${character}`);
+            dataList.push(result.data)
+        }
+        catch(e: any){
+          console.log(e.Message);
+        }
+        return dataList
+    }
+}
+
+//
+// helper methods -------------------------------
+
+export default {
+    getBooksList,
+    getBook,
+    getBookCharacters
+};
---
+++ src/controllers/comments.ts
@@ -0,0 +1,129 @@
+import {query, Request, Response} from "express";
+let mysql = require('mysql');
+const dotenv = require("dotenv")
+dotenv.config()
+let query_data: any = []
+let query_data_write: any = []
+
+const connectDb = async (data: any, read: boolean, write: boolean, destination: string) => {
+    try {
+        let dbconnection = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'test',
+            connectionLimit : 10,
+            debug:  false
+        });
+
+        if (read) {
+            dbconnection.getConnection(async function (err: any, connection:any) {
+                console.log('Connected to the MySQL --- server.read_db');
+                if (err) {
+                    return console.error('error: ' + err.message);
+                }
+                let param_data = data
+                let sql = "SELECT * FROM comments WHERE book_isbn=" + `'${param_data.isbn}'` + " ORDER BY created_at DESC";
+                connection.query(sql, function (err: any, result: any) {
+                    if (err) throw err;
+                    console.log(sql)
+                    query_data = result
+                    return result
+                })
+                connection.release();
+            });
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const connectDbWrite = async (data: any, read: boolean, write: boolean, destination: string) => {
+    try {
+        let connectionWrite = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'test',
+            connectionLimit : 10,
+            debug:  false
+        });
+
+        if (write) {
+             connectionWrite.getConnection(function (err: any,connection: any) {
+                 console.log('Connected to the MySQL --- server.write');
+                 if (err) {
+                    return console.error('error: ' + err.message);
+                }
+                let param_data = JSON.parse(data)
+                let sql_write = "INSERT INTO comments (comment, commenter_ip_adress, book_isbn) VALUES (" + `'${param_data.comment}'` + "," + `'${param_data.ip_address}'` + "," + `'${param_data.isbn}'` + ")";
+                 connection.query(sql_write, function (err: any, result: any) {
+                    if (err) throw err;
+                    console.log("1 record inserted");
+                    query_data_write = result
+                })
+                 connection.release();
+             });
+
+            connectionWrite.getConnection(function (err: any, connection: any) {
+                console.log("comments getConnection for write ");
+                if (err) {
+                    return console.error('error: ' + err.message);
+                }
+                let param_data = JSON.parse(data)
+                let sql_read = "SELECT * FROM comments WHERE book_isbn=" + `'${param_data.isbn}'` + " ORDER BY created_at DESC";
+                connection.query(sql_read, function (err: any, result: any) {
+                if (err) throw err;
+                console.log("comments list records retrieved ");
+                query_data = result
+                })
+                connection.release();
+            })
+
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const createComment = async (req: Request, res: Response) => {
+    let requestData = Object.keys(req.body)[0]
+    connectDbWrite(requestData,false,true,"comments").then(r => console.log(r))
+    return res.status(200).json({
+        status: true,
+        message:"Comment saved Successfully",
+        data: query_data_write
+    });
+};
+
+const updateComment = async (req: Request, res: Response) => {
+    connectDbWrite(req.body,false,true,"comments").then(r => console.log(r))
+    return res.status(200).json({
+        status: true,
+        message:"Comments Updated Successfully",
+    });
+};
+
+const deleteComment = async (req: Request, res: Response) => {
+    connectDbWrite(req.body,false,true,"comments").then(r => console.log(r))
+    return res.status(200).json({
+        status: true,
+        message:"Comment deleted Successfully",
+    });
+};
+
+const getComments = async (req: Request, res: Response) => {
+    connectDb(req.query,true,false,'comments').then(r => console.log(r))
+    return res.status(200).json({
+        status: true,
+        message: "Comments Listed",
+        data: query_data
+    });
+};
+
+export default {
+    getComments,
+    deleteComment,
+    updateComment,
+    createComment,
+}
