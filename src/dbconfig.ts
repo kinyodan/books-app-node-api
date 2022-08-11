31014d8989126b749f4fa1059831730af24ee636
Daniel Kinyo <kinyodanie@outlool.com>
2022-08-11 08:51:32 +0300

    update db config

--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -10,8 +10,7 @@ if(process.env.NODE_ENV === 'production'){
       user: process.env.PRODUCTION_DB_USER || 'b2eb7f5d522aef',
       password: process.env.PRODUCTION_DB_PASSWORD || '19ad1620',
       database: process.env.PRODUCTION_DATABASE || 'heroku_32f323172c70e52',
-      connectionLimit : 10,
-      debug:  false
+      debug:  true
     }
 }
 
