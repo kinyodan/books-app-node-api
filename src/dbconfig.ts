d5247accadc3fd87fcf3e439feef8dea436aa100
Daniel Kinyo <kinyodanie@outlool.com>
2022-08-11 08:44:25 +0300

    update db config

--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -6,10 +6,10 @@ let dbconfig: any = {}
 // Config production
 if(process.env.NODE_ENV === 'production'){
     dbconfig = {
-      host: process.env.PRODUCTION_DB_HOST || 'localhost',
-      user: process.env.PRODUCTION_DB_USER || 'root',
-      password: process.env.PRODUCTION_DB_PASSWORD || '',
-      database: process.env.PRODUCTION_DATABASE || 'test',
+      host: process.env.PRODUCTION_DB_HOST || 'us-cdbr-iron-east-02.cleardb.net',
+      user: process.env.PRODUCTION_DB_USER || 'b2eb7f5d522aef',
+      password: process.env.PRODUCTION_DB_PASSWORD || '19ad1620',
+      database: process.env.PRODUCTION_DATABASE || 'heroku_32f323172c70e52',
       connectionLimit : 10,
       debug:  false
     };
@@ -19,10 +19,10 @@ if(process.env.NODE_ENV === 'production'){
 if (process.env.NODE_ENV === 'development') {
     console.log("Config Development")
     dbconfig = {
-      host: process.env.LOCAL_DB_HOST || 'localhost',
-      user: process.env.LOCAL_DB_USER || 'root',
-      password: process.env.LOCAL_DB_PASSWORD || '',
-      database: process.env.LOCAL_DATABASE || 'test',
+      host: process.env.LOCAL_DB_HOST || 'us-cdbr-iron-east-02.cleardb.net',
+      user: process.env.LOCAL_DB_USER || 'b2eb7f5d522aef',
+      password: process.env.LOCAL_DB_PASSWORD || '19ad1620',
+      database: process.env.LOCAL_DATABASE || 'heroku_32f323172c70e52',
       connectionLimit: 10,
       debug: false
    };
