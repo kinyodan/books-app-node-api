49c0c110320f0a66524f287413c09e296716004a
Daniel Kinyo <kinyodanie@outlool.com>
2022-08-12 11:47:38 +0300

    updates update getting comment list

--- src/controllers/characters.ts
+++ src/controllers/characters.ts
@@ -2,12 +2,21 @@ import { Request, Response } from "express";
 import axios, { AxiosResponse } from "axios";
 
 const getCharactersList = async (req: Request, res: Response) => {
-    let requestData: any = JSON.parse(Object.keys(req.body)[0]);
-    let req_data = {
-        isbn: requestData.isbn,
-        sortBy: requestData.sortBy,
-        filters: requestData.filters,
-    };
+    let requestKeys = Object.keys(req.body)
+    let requestDatavalues = Object.values(req.body)
+    let req_data: any = {}
+
+    requestKeys.forEach(function (value, index) {
+        req_data[value] = requestDatavalues[index]
+    });
+
+    console.log( req_data)
+    // let requestData: any = JSON.parse(Object.keys(req.body));
+    // let req_data = {
+    //     isbn: requestData.isbn,
+    //     sortBy: requestData.sortBy,
+    //     filters: requestData.filters,
+    // };
 
     let result: AxiosResponse = await axios.post(
         `${process.env.SERVICE_API_URL}/batch_get_characters_details`,
@@ -32,15 +41,6 @@ const getAllCharactersList = async (req: Request, res: Response) => {
     requestKeys.forEach(function (value, index) {
         req_data[value] = requestDatavalues[index]
     });
-
-    // let requestData: any = JSON.parse(Object.keys(req.body)[0]);
-
-    // let req_data = {
-    //     isbn: requestData.isbn,
-    //     sortBy: requestData.sortBy,
-    //     filters: requestData.filters,
-    // };
-    console.log(req_data)
 
     let result: AxiosResponse = await axios.post(
         `${process.env.SERVICE_API_URL}/get_all_characters`,
