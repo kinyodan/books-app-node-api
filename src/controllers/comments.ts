37a792b364ad6eeb92718bfe4453bedd8328106a
Daniel Kinyo <kinyodanie@outlool.com>
2022-08-12 07:11:03 +0300

    updates add posgress to replace mysql: probelm connecting to mysql on heroku

--- src/controllers/comments.ts
+++ src/controllers/comments.ts
@@ -47,7 +47,7 @@ const connectDbWrite = async (data: any, read: boolean, write: boolean, destinat
             if (err) throw err;
             console.log("Connected! to pg-------------------------------------------------------------");
             const now = new Date();
-            console.log(now.toISOString);
+            console.log(now.toISOString());
 
             let sql_write = "INSERT INTO comments (comment, commenter_ip_address, book_isbn,created_at) VALUES (" + `'${data.comment}'` + "," + `'${data.ip_address}'` + "," + `'${data.isbn}'` + "," + `'to_timestamp(${now.toISOString} / 1000.0)'`+ ")";
             connection.query(sql_write, function (err: any, result: any) {
